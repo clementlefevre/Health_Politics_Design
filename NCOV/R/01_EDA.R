c5528fea16b5d5e9fa193a7eb3ad3a2f2e6b7f26
clementlefevre <clement.lefevre@deutschebahn.com>
2021-09-09 14:26:04 +0200

    Ass NULL check and implement case AGNOSTICISM.

--- NCOV/R/01_EDA.R
+++ NCOV/R/01_EDA.R
@@ -7,11 +7,9 @@ DT[, sum(is.na(.SD)),  .SDcols =names(DT)]
 colSums(!is.na(DT))
 
 
-#DT_count_UNK <- aggregate(.~CountryName, data=DT, FUN= function(x) (sum(x=="UNK")))
-#DT_count_all <- aggregate(.~CountryName, data=DT, FUN=length)
 
 count_UNK_percent <- function(x){
-  ratio <- sum(x=="UNK")/(sum(!is.na(x)))
+  ratio <- sum(tolower(x)=="unk")/(sum(!is.na(x)))
   return (round(ratio*100,2))
 }
 DT_count_UNK_percent <- aggregate(.~CountryName, data=DT,FUN =count_UNK_percent)
@@ -19,4 +17,17 @@ setDT(DT_count_UNK_percent)
 cols_to_sort <-  names(DT_count_UNK_percent)
 cols_to_sort <-  paste0(cols_to_sort, "_UNK_percent")
 names(DT_count_UNK_percent) <- cols_to_sort
-openxlsx::write.xlsx(DT_count_UNK_percent, "../data/output/NCOV_missing_data.xlsx")
+openxlsx::write.xlsx(DT_count_UNK_percent, "../data/output/NCOV_missing_data_UNK.xlsx")
+
+
+count_NULL_percent <- function(x){
+  ratio <- sum(tolower(x)=="null")/(sum(!is.na(x)))
+  return (round(ratio*100,2))
+}
+DT_count_NULL_percent <- aggregate(.~CountryName, data=DT,FUN =count_NULL_percent)
+setDT(DT_count_NULL_percent)
+cols_to_sort <-  names(DT_count_NULL_percent)
+cols_to_sort <-  paste0(cols_to_sort, "_NULL_percent")
+names(DT_count_NULL_percent) <- cols_to_sort
+openxlsx::write.xlsx(DT_count_NULL_percent, "../data/output/NCOV_missing_data_NULL.xlsx")
+
